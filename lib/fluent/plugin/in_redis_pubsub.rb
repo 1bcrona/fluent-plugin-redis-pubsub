a7a7bf2ee21ac31b6d7799ee770fc7189612c30c
Bağış Can Rona <Ba Can Rona>
2016-11-04 21:20:49 +0300

    id added

--- lib/fluent/plugin/in_redis_pubsub.rb
+++ lib/fluent/plugin/in_redis_pubsub.rb
@@ -8,12 +8,24 @@ module Fluent
     config_param :port, :integer, default:  6379
     config_param :channel, :string
     config_param :tag, :string, default: nil
+    config_param :id, :string, default:nil
 
     def initialize
       super
       require 'redis'
       require 'msgpack'
+      require 'socket'
     end
+
+
+    def ipv4_private
+      Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+    end
+
+    def ipv4_public
+      Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private?}
+    end
+  
 
     def configure(config)
       super
@@ -25,7 +37,20 @@ module Fluent
 
     def start
       super
-      @redis  = Redis.new(:host => @host, :port => @port ,:thread_safe => true)
+      @client_id = nil
+      @ip = nil
+      if config.has_key?('id')
+         @client_id =config['id'].to_s
+      else
+         if ipv4_public.nil?
+            @ip  = ipv4_private.ip_address
+         else
+            @ip = ipv4_public.ip_address
+         end
+            @client_id = "#{@ip}_#{@channel}"
+      end
+      
+      @redis  = Redis.new(:host => @host, :port => @port ,:id=>@client_id,:thread_safe => true)
       @thread = Thread.new(&method(:run))
     end
 
