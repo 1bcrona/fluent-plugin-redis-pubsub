993d7ed0f972e7a659ce55f9b70a833a144ec761
Bağış Can Rona <Ba Can Rona>
2016-11-05 10:44:15 +0300

    psubscribe to subscribe

--- lib/fluent/plugin/in_redis_pubsub.rb
+++ lib/fluent/plugin/in_redis_pubsub.rb
@@ -1,6 +1,6 @@
 module Fluent
-  class RedisPubsubInput < Input
-    Plugin.register_input('redis_pubsub', self)
+  class RedisPubsubInpt < Input
+    Plugin.register_input('redis_pubsub_f', self)
 
     attr_reader  :redis
 
@@ -33,34 +33,38 @@ module Fluent
       @port    = config.has_key?('port')    ? config['port'].to_i    : 6379
       raise Fluent::ConfigError, "need channel" if not config.has_key?('channel') or config['channel'].empty?
       @channel = config['channel'].to_s
-    end
-
-    def start
-      super
       @client_id = nil
-      @ip = nil
+      ip = nil
       if config.has_key?('id')
          @client_id =config['id'].to_s
       else
          if ipv4_public.nil?
-            @ip  = ipv4_private.ip_address
+            ip  = ipv4_private.ip_address
          else
-            @ip = ipv4_public.ip_address
+            ip = ipv4_public.ip_address
          end
-            @client_id = "#{@ip}_#{@channel}"
+            @client_id = "#{ip}_#{@channel}"
       end
-      
-      @redis  = Redis.new(:host => @host, :port => @port ,:id=>@client_id,:thread_safe => true)
+
+
+    end
+
+    def start
+      super
+      $log.info "Client name  => #{@client_id}"
+      @redis  = Redis.new(:id => @client_id , :host => @host, :port => @port)
+
+      $log.info "#{@redis.inspect}"
       @thread = Thread.new(&method(:run))
     end
 
     def run
-      @redis.psubscribe @channel do |on|
-        on.psubscribe do |channel, subscriptions|
+      @redis.subscribe @channel do |on|
+        on.subscribe do |channel, subscriptions|
           $log.info "Subscribed to ##{channel} (#{subscriptions} subscriptions)"
         end
 
-        on.pmessage do |pattern, channel, msg|
+        on.message do |channel, msg|
           parsed = nil
           begin
             parsed = JSON.parse msg
@@ -78,3 +82,4 @@ module Fluent
     end
   end
 end
+
